561c1c93da2e08402f4ffee0dcbadd600897fc0a
hinst <hinstance@yandex.ru>
2018-11-11 13:02:41 +0100

    New version info panel

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,16 @@
 console.log('Now starting app...')
-
 const Vue = require('vue/dist/vue.common.js')
-
 const app = new Vue({
     el: '#app',
     data: {
-        node_version: process.versions.node
+        nodeVersion: process.versions.node,
+        chromeVersion: process.versions['chrome'],
+        electronVersion: process.versions['electron'],
+        debugInfoVisible: false,
+    },
+    methods: {
+        toggleDebugInfo: function() {
+            this.debugInfoVisible = !this.debugInfoVisible;
+        }
     }
 })
